2f69c24aae7d3adcddf9016cfba239d6dbd19ed5
Andrew Plaza <aplaza@liquidthink.net>
2018-07-15 01:09:25 -0400

    made enact_result function of executive_ext public

--- ethcore/src/executive.rs
+++ ethcore/src/executive.rs
@@ -684,7 +684,7 @@ impl<'a, B: 'a + StateBackend> Executive<'a, B> {
 		}
 	}
 
-	fn enact_result(&mut self, result: &vm::Result<FinalizationResult>, substate: &mut Substate, un_substate: Substate) {
+	pub fn enact_result(&mut self, result: &vm::Result<FinalizationResult>, substate: &mut Substate, un_substate: Substate) {
 		match *result {
 			Err(vm::Error::OutOfGas)
 				| Err(vm::Error::BadJumpDestination {..})
